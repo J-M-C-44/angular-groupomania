d6f8b56d7fdedd8c92c58f9b48f0b1cce0cf8661
J-M-C-44 <jean.michel.cozic@net-c.fr>
2022-10-07 13:10:55 +0200

    1er pas avant intégration deangular  material

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersRoutingModule } from './users-routing.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserDetailsComponent
+  ],
+  imports: [
+    CommonModule,
+    UsersRoutingModule, 
+  ]
+})
+export class UsersModule { }
